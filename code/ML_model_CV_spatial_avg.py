2f5dcf1b85b08cc1a28c074db136053481776061
anna-boser <46419226+anna-boser@users.noreply.github.com>
2024-02-15 14:32:10 -1000

    Update ML_model_CV_spatial_avg.py

--- code/ML_model_CV_spatial_avg.py
+++ code/ML_model_CV_spatial_avg.py
@@ -6,69 +6,69 @@ import gc
 import os
 import glob
 
-# df = pd.read_csv("Data/datasets/Train.csv")
+df = pd.read_csv("Data/datasets/Train.csv")
 
-# def cv(df):
-#     n_fold = len(set(df['Id']))
-#     kf = GroupKFold(n_fold)
-#     split = kf.split(df, groups=df['Id'])
+def cv(df):
+    n_fold = len(set(df['Id']))
+    kf = GroupKFold(n_fold)
+    split = kf.split(df, groups=df['Id'])
 
-#     metric_df = pd.DataFrame(columns=['station', 'Day', 'PM', 'PM_pred', 'rmse', 'bias'])
+    metric_df = pd.DataFrame(columns=['station', 'Day', 'PM', 'PM_pred', 'rmse', 'bias'])
 
-#     for i, (train_idx, test_idx) in enumerate(split):
-#         print(f'Starting training fold {i}.')
-#         _ = gc.collect()
+    for i, (train_idx, test_idx) in enumerate(split):
+        print(f'Starting training fold {i}.')
+        _ = gc.collect()
 
-#         # Calculate the daily mean PM for each day, excluding the current validation set
-#         validation_station = df.loc[test_idx, 'Id'].iloc[0]
-#         train_df = df[~df['Id'].isin([validation_station])]
-#         daily_mean_PM_excl_validation = train_df.groupby('Day')['PM'].mean()
+        # Calculate the daily mean PM for each day, excluding the current validation set
+        validation_station = df.loc[test_idx, 'Id'].iloc[0]
+        train_df = df[~df['Id'].isin([validation_station])]
+        daily_mean_PM_excl_validation = train_df.groupby('Day')['PM'].mean()
 
-#         # Add the daily mean PM (excluding validation station) as a feature to the original df
-#         df['Daily_Mean_PM_Excl_Val'] = df['Day'].map(daily_mean_PM_excl_validation)
+        # Add the daily mean PM (excluding validation station) as a feature to the original df
+        df['Daily_Mean_PM_Excl_Val'] = df['Day'].map(daily_mean_PM_excl_validation)
 
-#         features = ['Day', 'Lat', 'Lon', 'Elevation', 'Emissions', 'Forest',
-#                     'Roads', 'Streets', 'Plumes_High', 'Plumes_Med', 'Plumes_Low',
-#                     'Max_Temp', 'Max_Wind', 'Precip', 'Rel_Humidity', 'Wind_Dir', 'BLH',
-#                     'AOD', 'Daily_Mean_PM_Excl_Val']  # Include the new feature
+        features = ['Day', 'Lat', 'Lon', 'Elevation', 'Emissions', 'Forest',
+                    'Roads', 'Streets', 'Plumes_High', 'Plumes_Med', 'Plumes_Low',
+                    'Max_Temp', 'Max_Wind', 'Precip', 'Rel_Humidity', 'Wind_Dir', 'BLH',
+                    'AOD', 'Daily_Mean_PM_Excl_Val']  # Include the new feature
 
-#         # Adjust y by subtracting the mean PM for each day (excluding validation station)
-#         df['Adjusted_PM'] = df['PM'] - df['Daily_Mean_PM_Excl_Val']
-#         label_column = 'Adjusted_PM'
+        # Adjust y by subtracting the mean PM for each day (excluding validation station)
+        df['Adjusted_PM'] = df['PM'] - df['Daily_Mean_PM_Excl_Val']
+        label_column = 'Adjusted_PM'
 
-#         X = df[features]
-#         y = df[label_column]
+        X = df[features]
+        y = df[label_column]
 
-#         # Ensure the index alignment between X and y when slicing
-#         train_data = TabularDataset(pd.concat([X.loc[train_idx], y.loc[train_idx]], axis=1))
-#         test_data = TabularDataset(pd.concat([X.loc[test_idx], y.loc[test_idx]], axis=1))
+        # Ensure the index alignment between X and y when slicing
+        train_data = TabularDataset(pd.concat([X.loc[train_idx], y.loc[train_idx]], axis=1))
+        test_data = TabularDataset(pd.concat([X.loc[test_idx], y.loc[test_idx]], axis=1))
 
-#         predictor = TabularPredictor(label=label_column, eval_metric='r2').fit(train_data=train_data)
+        predictor = TabularPredictor(label=label_column, eval_metric='r2').fit(train_data=train_data)
         
-#         # task.fit(train_data=train_data, 
-#         #                     label=label_column, 
-#         #                     hyperparameters=hyperparameters, 
-#         #                     # time_limits = 60*60, # un-comment these for the tuned neural network. Train for 1 hour. 
-#         #                     # hyperparameter_tune=True, 
-#         #                     eval_metric='r2')
+        # task.fit(train_data=train_data, 
+        #                     label=label_column, 
+        #                     hyperparameters=hyperparameters, 
+        #                     # time_limits = 60*60, # un-comment these for the tuned neural network. Train for 1 hour. 
+        #                     # hyperparameter_tune=True, 
+        #                     eval_metric='r2')
 
-#         y_test = test_data[label_column]
-#         test_data_nolab = test_data.drop(labels=[label_column], axis=1)
-#         y_pred = predictor.predict(test_data_nolab)
+        y_test = test_data[label_column]
+        test_data_nolab = test_data.drop(labels=[label_column], axis=1)
+        y_pred = predictor.predict(test_data_nolab)
 
-#         # Calculate metrics
-#         bias = y_pred - y_test
-#         rmse = np.abs(bias)
+        # Calculate metrics
+        bias = y_pred - y_test
+        rmse = np.abs(bias)
 
-#         station = np.repeat(df.loc[test_idx]['Id'].iloc[0], len(test_data['Day']))
-#         df_to_append = pd.DataFrame({'station': station, 'Day': test_data['Day'], 'PM': y_test + df.loc[test_idx, 'Daily_Mean_PM_Excl_Val'], 'PM_pred': y_pred + df.loc[test_idx, 'Daily_Mean_PM_Excl_Val'], 'rmse': rmse, 'bias': bias})
+        station = np.repeat(df.loc[test_idx]['Id'].iloc[0], len(test_data['Day']))
+        df_to_append = pd.DataFrame({'station': station, 'Day': test_data['Day'], 'PM': y_test + df.loc[test_idx, 'Daily_Mean_PM_Excl_Val'], 'PM_pred': y_pred + df.loc[test_idx, 'Daily_Mean_PM_Excl_Val'], 'rmse': rmse, 'bias': bias})
 
-#         metric_df = pd.concat([metric_df, df_to_append], ignore_index=True)
+        metric_df = pd.concat([metric_df, df_to_append], ignore_index=True)
 
-#     return metric_df
+    return metric_df
 
-# AutoGluon = cv(df)
-# AutoGluon.to_csv("Data/output/CV/AutoGluon_spatial_var.csv", index = False)
+AutoGluon = cv(df)
+AutoGluon.to_csv("Data/output/CV/AutoGluon_spatial_var.csv", index = False)
 
 small_ids = pd.read_csv("Data/datasets/Small_Ids.csv")['x']
 small_ids = [float(i) for i in small_ids]
